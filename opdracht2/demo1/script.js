d04d77dea93c960cf75371cfcf669cf7a20dbd20
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-03-23 11:30:06 +0100

    Beide prototypes werkend

--- opdracht2/demo1/script.js
+++ opdracht2/demo1/script.js
@@ -1,22 +1,75 @@
+
+
 // Source: https://gist.github.com/Jiert/bbddb482c28f6c79cccc
 
 (function(){
+
+    // var app = {
+    //     tabs: function() {
+    //         var tabNav = document.getElementsByClassName('tab');
+    //         var tabContent = document.getElementsByClassName('content');
+            
+    //         for (var i=0; i < tabNav.length; i++){
+    //             tabNav[i].addEventListener('click', function(href) { 
+    //                 var href = this.getAttribute('href');
+    //                 var clearHref = href.replace('#', '')
+    //                 showContent(clearHref);
+    //             });
+    //             // console.log(href);
+
+    //             function showContent(clearHref) {
+    //                 console.log(clearHref); 
+    //                 for (var i=0; i < tabContent; i++){
+    //                     if (clearHref === tabContent[i].id) {
+    //                         tabContent[i].style.display = 'block';
+    //                     } else {
+    //                         tabContent[i].style.display = 'none';
+    //                     }
+    //                 }
+    //             }
+    //             showContent('tab-1');
+    //         }
+    //     }
+    // }
+    // app.tabs()
+
     console.log('hai');
 
     var el = document.getElementById('tabs');
+
+    // function addStyle() {
+        // for (var i=0; i < selectContent.length; i++) {
+        //     if (selectContent[i].classList.contains('active') && selectContent[i].classList.contains('content')) {
+        //         selectContent[i].style.display = 'block';
+        //     } else {
+        //         selectContent[i].style.display = 'none';
+        //     }
+        // }   
+    // }
 
     function onTabClick(event){
         console.log('ik doe het ook');
 
         var selectActives = document.querySelectorAll('.active');
+        var selectContent = document.querySelectorAll('.content');
 
         // deactivate existing active tab and panel
         for (var i=0; i < selectActives.length; i++){
+            console.log(selectActives)
             selectActives[i].className = selectActives[i].className.replace(' active', '');
         }
 
+        // for (var i=0; i < selectContent.length; i++) {
+        //     console.log(selectContent[i].classList.contains('active'));
+        //     if (selectContent[i].classList.contains('active') && selectContent[i].classList.contains('content')) {
+        //         selectContent[i].style.display = 'block';
+        //     } else {
+        //         selectContent[i].style.display = 'none';
+        //     }
+        // }  
+
         // activate new tab and panel
-        event.target.parentElement.className += ' active';
+        event.target.className += ' active';
         document.getElementById(event.target.href.split('#')[1]).className += ' active';
 
         event.preventDefault();
