c196698cbdd0b825a359d6d3a8a5229fcaf753e0
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-03-23 16:06:12 +0100

    Comments weggehaald in 'script.js'

--- opdracht2/demo1/script.js
+++ opdracht2/demo1/script.js
@@ -2,49 +2,10 @@
 
 (function(){
 
-    // var app = {
-    //     tabs: function() {
-    //         var tabNav = document.getElementsByClassName('tab');
-    //         var tabContent = document.getElementsByClassName('content');
-            
-    //         for (var i=0; i < tabNav.length; i++){
-    //             tabNav[i].addEventListener('click', function(href) { 
-    //                 var href = this.getAttribute('href');
-    //                 var clearHref = href.replace('#', '')
-    //                 showContent(clearHref);
-    //             });
-    //             // console.log(href);
-
-    //             function showContent(clearHref) {
-    //                 console.log(clearHref); 
-    //                 for (var i=0; i < tabContent; i++){
-    //                     if (clearHref === tabContent[i].id) {
-    //                         tabContent[i].style.display = 'block';
-    //                     } else {
-    //                         tabContent[i].style.display = 'none';
-    //                     }
-    //                 }
-    //             }
-    //             showContent('tab-1');
-    //         }
-    //     }
-    // }
-    // app.tabs()
-
     console.log('hai');
 
     var el = document.getElementById('tabs');
 
-    // function addStyle() {
-        // for (var i=0; i < selectContent.length; i++) {
-        //     if (selectContent[i].classList.contains('active') && selectContent[i].classList.contains('content')) {
-        //         selectContent[i].style.display = 'block';
-        //     } else {
-        //         selectContent[i].style.display = 'none';
-        //     }
-        // }   
-    // }
-    
     function addStyle() {
         var selectContent = document.querySelectorAll('.content');
         
