a6fbcb07e99048124286d1a25e4961bf8c2206f4
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-05-30 12:23:35 +0200

    Changes JS

--- opdracht3/wt3-script.js
+++ opdracht3/wt3-script.js
@@ -67,33 +67,33 @@ if (document.querySelectorAll && document.body.classList && document.body.classL
     addContent();
 
     // Listen for click on the document
-    // document.addEventListener('click', function (event) {
+    document.addEventListener('click', function (event) {
     
-    //     //Bail if our clicked element doesn't have the class
-    //     if (!event.target.classList.contains('accordion-toggle')) return;
+        //Bail if our clicked element doesn't have the class
+        if (!event.target.classList.contains('accordion-toggle')) return;
         
-    //     // Get the target content
-    //     var content = document.querySelector(event.target.hash);
-    //     if (!content) return;
+        // Get the target content
+        var content = document.querySelector(event.target.hash);
+        if (!content) return;
         
-    //     // Prevent default link behavior
-    //     event.preventDefault();
+        // Prevent default link behavior
+        event.preventDefault();
         
-    //     // If the content is already expanded, collapse it and quit
-    //     if (content.classList.contains('active')) {
-    //         content.classList.remove('active');
-    //         return;
-    //     }
+        // If the content is already expanded, collapse it and quit
+        if (content.classList.contains('active')) {
+            content.classList.remove('active');
+            return;
+        }
         
-    //     // Get all open accordion content, loop through it, and close it
-    //     var accordions = document.querySelectorAll('.accordion-content.active');
-    //         for (var i = 0; i < accordions.length; i++) {
-    //         accordions[i].classList.remove('active');
-    //     }
+        // Get all open accordion content, loop through it, and close it
+        var accordions = document.querySelectorAll('.accordion-content.active');
+            for (var i = 0; i < accordions.length; i++) {
+            accordions[i].classList.remove('active');
+        }
         
-    //     // Toggle our content
-    //     content.classList.toggle('active');
-    // })
+        // Toggle our content
+        content.classList.toggle('active');
+    })
 }
 
 // Search function
